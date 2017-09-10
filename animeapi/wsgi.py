25b7e8d2075656df32f42b516a5358d427628e85
Keita <bonjovi1926@yahoo.co.jp>
2017-09-10 14:17:45 +0900

    remove: thread

--- animeapi/wsgi.py
+++ animeapi/wsgi.py
@@ -8,15 +8,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-import threading
-import api.resident as r
-
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "animeapi.settings")
 
 application = get_wsgi_application()
-
-t = threading.Thread(target=r.process)
-t.start()
