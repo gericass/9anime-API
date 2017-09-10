c1806589cce25a2377c095ca41224a16f41076e0
Keita <bonjovi1926@yahoo.co.jp>
2017-09-10 15:01:13 +0900

    fix

--- animeapi/wsgi.py
+++ animeapi/wsgi.py
@@ -8,8 +8,8 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-import api.resident as logger
-import threading
+#import api.resident as logger
+#import threading
 
 from django.core.wsgi import get_wsgi_application
 
