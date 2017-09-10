2d96c71e0122a3c6dfb6a583e1efab48a8e43801
Keita <bonjovi1926@yahoo.co.jp>
2017-09-10 14:42:20 +0900

    test

--- api/resident.py
+++ api/resident.py
@@ -3,24 +3,28 @@ import api.scraping as sc
 import api.day_of_week as DoW
 
 def process():
-    season = ["Winter","Spring","Summer","Fall","Unknow"]
-    page = [1,2,3,4,5]
-    page2 = [1,2]
-    year = [2017]#,2016,2015,2014,2013]
     while True:
-        for i in year:
-            for j in season:
-                for k in page:
-                    try:
-                        sc.scrapingreq(i,k,j)
-                    except:
-                        print(str(i)+j+str(k)+" error")
-        for i in page2:
-            for j in year:
-                for k in season:
-                    for m in page:
-                        try:
-                            DoW.getweekday(j,m,k,i)
-                        except:
-                            print(str(i)+str(j)+k+str(m)+" error")
-        time.sleep(86400) #一日(86400秒)おきに実行
+        print("yes")
+        time.sleep(5)
+
+    #season = ["Winter","Spring","Summer","Fall","Unknow"]
+    #page = [1,2,3,4,5]
+    #page2 = [1,2]
+    #year = [2017]#,2016,2015,2014,2013]
+    #while True:
+    #    for i in year:
+    #        for j in season:
+    #            for k in page:
+    #                try:
+    #                    sc.scrapingreq(i,k,j)
+    #                except:
+    #                    print(str(i)+j+str(k)+" error")
+    #    for i in page2:
+    #        for j in year:
+    #            for k in season:
+    #                for m in page:
+    #                    try:
+    #                        DoW.getweekday(j,m,k,i)
+    #                    except:
+    #                        print(str(i)+str(j)+k+str(m)+" error")
+    #    time.sleep(86400) #一日(86400秒)おきに実行
