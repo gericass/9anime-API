54a159cbfca3bb9b6b148bd2239f27e8d70813cc
Keita <bonjovi1926@yahoo.co.jp>
2017-09-10 14:03:46 +0900

    fix

--- animeapi/wsgi.py
+++ animeapi/wsgi.py
@@ -17,8 +17,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "animeapi.settings")
 
 application = get_wsgi_application()
-
-'''
-t = threading.Thread(target=r.process)
-t.start()
-'''
