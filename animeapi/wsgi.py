513f9f558f9a855211b47eeed9a2b17ffa85cb41
Keita <bonjovi1926@yahoo.co.jp>
2017-09-10 15:06:35 +0900

    fix

--- animeapi/wsgi.py
+++ animeapi/wsgi.py
@@ -8,14 +8,23 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-#import api.resident as logger
-#import threading
+import api.resident as logger
+import time
+import threading
+
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "animeapi.settings")
 
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-#t = threading.Thread(target=logger.process)
-#t.start()
+
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
+
+t = threading.Thread(target=logger.process)
+t.start()
