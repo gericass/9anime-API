3521e0947005b7db1cd447fc6607031a077e8e8a
Keita <bonjovi1926@yahoo.co.jp>
2017-09-10 15:26:03 +0900

    add: process

--- animeapi/wsgi.py
+++ animeapi/wsgi.py
@@ -11,14 +11,34 @@ import os
 import time
 #import api.resident こいつインポートするとバグる
 import threading
+import api.scraping as sc
+import api.day_of_week as DoW
 
 from django.core.wsgi import get_wsgi_application
 
 
 def process():
+    season = ["Winter","Spring","Summer","Fall","Unknow"]
+    page = [1,2,3,4,5]
+    page2 = [1,2]
+    year = [2017]#,2016,2015,2014,2013]
     while True:
-        print("LOG!!!!!!!!!!!!!!!!!!!!")
-        time.sleep(5)
+        for i in year:
+            for j in season:
+                for k in page:
+                    try:
+                        sc.scrapingreq(i,k,j)
+                    except:
+                        print(str(i)+j+str(k)+" error")
+        for i in page2:
+            for j in year:
+                for k in season:
+                    for m in page:
+                        try:
+                            DoW.getweekday(j,m,k,i)
+                        except:
+                            print(str(i)+str(j)+k+str(m)+" error")
+        time.sleep(86400) #一日(86400秒)おきに実行
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "animeapi.settings")
 
